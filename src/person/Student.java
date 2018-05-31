c4a10bf603d619c8e0aa3eef46218c91157b1610
flackery <andrushagalushka@gmail.com>
2018-05-31 13:53:07 +0300

    realized knowlage and student class

---
+++ src/person/Student.java
@@ -0,0 +1,15 @@
+package person;
+
+import person.consciousness.Knowledge;
+
+public class Student {
+    String name;
+    Knowledge knowledge;
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void setKnowledge(Knowledge knowledge) {
+        this.knowledge = knowledge;
+    }
+}
---
+++ src/person/consciousness/Knowledge.java
@@ -0,0 +1,8 @@
+package person.consciousness;
+
+public class Knowledge {
+    int level;
+    public Knowledge(int level) {
+        this.level = level;
+    }
+}
