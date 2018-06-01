d3832cf987d60a4e91a1279d90d88aaf9f054d34
flackery <andrushagalushka@gmail.com>
2018-06-01 12:26:04 +0300

    realize :X

--- src/main/Application.java
+++ src/main/Application.java
@@ -7,11 +7,16 @@ import person.Student;
 public class Application {
     public static void main(String[] args) {
         University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
+        university.addStudent(new Student("Andrew Kostenko"), new Knowledge(4));
+        university.addStudent(new Student("Julia Veselkina"), new Knowledge(2));
+        university.addStudent(new Student("Maria Perechrest"), new Knowledge(5));
 
         Internship internship = new Internship("Interlink");
+        for (Student student : university.getStudentList()) {
+            if (student.getKnowledge().getLevel() > 3) {
+                internship.setStudent(student);
+            }
+        }
         System.out.println("List of internship's students:");
         System.out.println(internship.getStudents());
     }
